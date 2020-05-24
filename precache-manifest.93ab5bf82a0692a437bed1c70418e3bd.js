3b78b88937416021473926d12d01fa14e003c3c7
j.m.dola <John@pauldederericloud.onmicrosoft.com>
2020-05-24 12:30:42 +0200

    Updates

---
+++ precache-manifest.93ab5bf82a0692a437bed1c70418e3bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d11ddb710bd1d9987d3a8b25eb53929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63ba843e0b0c50a71827",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "687eeb407d9d8bcddd9f",
+    "url": "/static/js/2.d4e981b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4e981b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ba843e0b0c50a71827",
+    "url": "/static/js/main.ff0a1309.chunk.js"
+  },
+  {
+    "revision": "79d6144e79aee8501249",
+    "url": "/static/js/runtime-main.a7e334ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
