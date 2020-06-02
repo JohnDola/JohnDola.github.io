50fb5602adea48fc462eece0499a7e5400a2270c
JDola <jda@mm-software.com>
2020-06-02 14:38:31 +0200

    Added Correct Animation for skills

--- src/components/MySkills/Skill/Skill.js
+++ src/components/MySkills/Skill/Skill.js
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import classes from './Skill.module.css';
+import { ESection } from '../../../common/sectionTitles';
 const Skill = ({ name, percentage }) => {
-  let [delayedPercentage ,setDelayedPercentage]= useState(0);
-  setTimeout(()=>{
-    setDelayedPercentage( percentage)
-  },300)
+	let [ delayedPercentage, setDelayedPercentage ] = useState(0);
 
-  return (
-    
-    <div className={classes.Skill}>
-      <h6>
-        {name}
-        <span>{percentage} %</span>
-      </h6>
-      <div className={classes.PercentageBar}>
-        <span style={{width: delayedPercentage + '%'}}></span>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			if (document.documentElement.scrollTop > 100) {
+				setDelayedPercentage(percentage);
+			} else {
+				setDelayedPercentage(0);
+			}
+		});
+	}, []);
+	return (
+		<div className={classes.Skill}>
+			<h6>
+				{name}
+				<span>{percentage} %</span>
+			</h6>
+			<div className={classes.PercentageBar}>
+				<span style={{ width: delayedPercentage + '%' }} />
+			</div>
+		</div>
+	);
 };
 export default Skill;
