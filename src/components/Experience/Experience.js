8e6b719c56a3b17f80cb06090d42e81cad6f1f71
JDola <jda@mm-software.com>
2020-06-03 08:28:28 +0200

    Fixed Shadow for boxes

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -14,17 +14,15 @@ const Experience = (props) => {
                  ullamco ad ipsum excepteur non adipisicing dolore Lorem aliqua mollit aliqua consectetur nisi. 
                  In magna ipsum ipsum ut ea. Nisi dolor dolore irure officia est officia id consectetur adipisicing sint nisi.
                   Minim ullamco est elit ex. Labore pariatur voluptate aute velit ullamco.'`,
-            responsibilities: ['Frontend', 'Backend','']
+			responsibilities: ['JavaScript/TypeScript', 'HTML/CSS','ExtJS','ExtReact','C#','CefSharp', 'Cordova'],
+			experiences:['Great Job in reputable company', 'interesting projects']
 		}
 	];
 	return (
 		<section id={ESection.experience} className={classes.Experience}>
 			{/* TODO <img></img> */}
 			<h2>Experience</h2>
-
-			<ExperienceTrack />
-			<ExperienceTrack />
-			<ExperienceTrack />
+			{tracks.map((track,index) => <ExperienceTrack key={index}{...track} ></ExperienceTrack>)}
 		</section>
 	);
 };
--- src/components/Experience/ExperienceTrack/ExperienceTrack.js
+++ src/components/Experience/ExperienceTrack/ExperienceTrack.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import classes from './ExperienceTrack.module.css';
-const ExperienceTrack = (props) => (
+const ExperienceTrack = ({ title, responsibilities, description, location, experiences }) => (
 	<div className={classes.ExperienceTrack}>
-		
+		<aside>
+			<h4>{title}</h4>
+			<big>{location}</big>
+		</aside>
+		<div className={classes.Listing}>
+			<p>{description}</p>
+			<ul>{responsibilities.map((entry, index) => <li key={index}>{entry}</li>)}</ul>
+			<ul>{experiences.map((entry, index) => <li key={index}>{entry}</li>)}</ul>
+		</div>
 	</div>
 );
 
